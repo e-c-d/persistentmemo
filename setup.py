6c11e72836728a0fb08111338760cd6731ce974d
Eduard Christian Dumitrescu <edumi042@uottawa.ca>
2015-07-24 20:11:37 -0400

    use setuptools

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from distutils.core import setup
-setup(name         =  "persistentmemo",
+from setuptools import setup
+pkg = "persistentmemo"
+setup(name         =  pkg,
       version      = "1.0.0",
       description  = "Persistent memoization library",
       author       = "Eduard Christian Dumitrescu",
       license      = "MIT",
       url          = "https://github.com/e-c-d/persistentmemo",
-      packages     = ["persistentmemo"],
+      packages     = [pkg],
       classifiers  = ["Programming Language :: Python :: 3 :: Only",
                       "Topic :: Software Development :: Libraries :: Python Modules",
                       "Topic :: Software Development :: Build Tools"])
